5d73635479e8e3dfa4922efd0e79f6cac0fade1a
Tom McLeod <emus.attack@gmail.com>
2013-03-09 17:59:05 -0500

    small bug fix

--- analyze.rb
+++ analyze.rb
@@ -7,9 +7,15 @@ puts ARGV.length
 
 ARGV.each_with_index do |csv_file,index|
 	STDERR.puts "Processing #{csv_file}"
+
+	temptime = []
+	tempstate = []
 	CSV.foreach(csv_file,headers: true) do |row|
-		waveforms[index] = Waveform.new(row["timestamp (abs)"],row["Channel-0"])
+		#waveforms[index] = Waveform.new(row["timestamp (abs)"],row["Channel-0"])
+		temptime << row["timestamp (abs)"]
+		tempstate << row["Channel-0"]
 	end
+	waveforms[index] = Waveform.new(temptime,tempstate)
 end
 
 puts "We have #{waveforms.length} waveforms to analyze"
